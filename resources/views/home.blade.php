425d8ee7aab60d86c1aec2e309fa75c3d86772a6
Luca <memeoluca6@gmail.com>
2021-05-07 17:31:30 +0200

    added container__cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,20 @@
             <div class="container">
                 {{-- sezione pasta lunga --}}
                 <section class="lunghe">
-                    
+                    <h2>Le lunghe</h2>
+                    <div class="container__cards">
+                        <div class="card"></div>   
+                    </div>
                 </section>
                 {{-- /sezione pasta lunga --}}
                 {{-- sezione pasta corta --}}
                 <section class="corte">
-    
+                    <h2>Le corte</h2>
                 </section>
                 {{-- /sezione pasta corta --}}
                 {{-- sezione pasta cortissima --}}
                 <section class="cortissime">
-    
+                    <h2>Le cortissime</h2>
                 </section>
                 {{-- /sezione pasta cortissima --}}
             </div>
