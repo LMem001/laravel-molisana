67edecea825d388e9f7988ff9343ecd7f19c1c9f
Luca <memeoluca6@gmail.com>
2021-05-07 16:52:56 +0200

    home base structure added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,25 @@
 <body>
     @include('parts.header')
     <main>
-        
+        <div id="home">
+            <div class="container">
+                {{-- sezione pasta lunga --}}
+                <section class="lunghe">
+                    
+                </section>
+                {{-- /sezione pasta lunga --}}
+                {{-- sezione pasta corta --}}
+                <section class="corte">
+    
+                </section>
+                {{-- /sezione pasta corta --}}
+                {{-- sezione pasta cortissima --}}
+                <section class="cortissime">
+    
+                </section>
+                {{-- /sezione pasta cortissima --}}
+            </div>
+        </div>
     </main>
 </body>
 </html>
