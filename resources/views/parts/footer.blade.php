d5e226ea3c70f156a00b9e6199c62d1969876c45
Luca <memeoluca6@gmail.com>
2021-05-09 14:36:27 +0200

    footer completed

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -8,7 +8,13 @@
                     <li>Regione sociale: La Molisana S.P.A</li>
                     <li>Sede legale: Contrada Colle delle Api, 100/A 86100 - campobasso</li>
                     <li>Pec: lamolisana@pec.it</li>
-                    <li></li>
+                    <li>Tel: +3908744981</li>
+                    <li>Fax: +390874629584</li>
+                    <li>info@lamolisana.it(per segnalazioni degli utenti)</li>
+                    <li>commerciale@lamolisana.it</li>
+                    <li>export@lamolisana.it</li>
+                    <li>numero verde 800818081</li>
+                    <li>telefono 3801292455</li>
                 </ul>
             </div>
             {{-- /copyright --}}
@@ -18,15 +24,40 @@
                 <div class="footer__navbars__first_half">
                     <nav>
                         <h5>Pastificio</h5>
+                        <ul>
+                            <li><a href="#">Il Pastificio</a></li>
+                            <li><a href="#">Grano decorticato a pietra</a></li>
+                            <li><a href="#">Il Molise c'è</a></li>
+                            <li><a href="#">Filiera integrata</a></li>
+                            <li><a href="#">100 anni di pasta</a></li>
+                            <li><a href="#">Sartoria della pasta</a></li>
+                            <li><a href="#">Spaghetto Quadrato</a></li>
+                            <li><a href="#">Le Persone</a></li>
+                        </ul>
                     </nav>
                     <nav>
                         <h5>Prodotti</h5>
+                        <ul>
+                            <li><a href="#">Le Classiche</a></li>
+                            <li><a href="#">Le Integrali</a></li>
+                            <li><a href="#">Le Speciali</a></li>
+                            <li><a href="#">Le Biologiche</a></li>
+                            <li><a href="#">Le Gluten-Free</a></li>
+                            <li><a href="#">Le Semole</a></li>
+                            <li><a href="#">Le Extra di Lusso</a></li>
+                        </ul>
                     </nav>
                 </div>
                 {{-- /left side --}}
                 {{-- right side --}}
                 <nav>
                     <h5>Collezione da chef</h5>
+                    <ul>
+                        <li><a href="#">Collezione da chef</a></li>
+                        <li><a href="#">Grandi Cucine</a></li>
+                        <li><a href="#">Biologiche</a></li>
+                        <li><a href="#">Quadrate</a></li>
+                    </ul>
                 </nav>
                 {{-- /right side --}}
             </div>
