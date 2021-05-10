9b198d3bc92af3e6f14962668a8de7dbcaaff393
Luca <memeoluca6@gmail.com>
2021-05-10 16:43:53 +0200

    product base structure added

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,5 +2,8 @@
 @section('content')
 <div class="product">
     <h1>{{ $pasta['titolo'] }}</h1>
+    <img src="{{$pasta['src-h']}}">
+    <img src="{{$pasta['src-p']}}" alt="$pasta['titolo']">
+    <p>{!!$pasta["descrizione"]!!}</p>
 </div>
 @endsection
